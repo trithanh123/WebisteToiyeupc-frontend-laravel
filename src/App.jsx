e6b6ce11c7bc5b08c63bc4e547c668bc34a8e07e
trithanh123 <phamtrithanh1234@gmail.com>
2026-05-23 05:00:02 +0700

    Khoi tao du an Frontend React va Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind đã lên sóng! Website ToiYeuPC sẵn sàng!
+      </h1>
+    </div>
+  )
+}
+
+export default App
